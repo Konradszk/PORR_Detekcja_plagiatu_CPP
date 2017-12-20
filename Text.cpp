ebbec0c60e3b25fc073c58bc3d0e9e319de06609
Konradszk <konradszk@gmail.com>
2017-12-20 22:29:29 +0100

    Initial commit

---
+++ Text.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lenovo on 20.12.2017.
+//
+
+#include "Text.h"
---
+++ Text.h
@@ -0,0 +1,14 @@
+//
+// Created by Lenovo on 20.12.2017.
+//
+
+#ifndef PORR_TEXT_H
+#define PORR_TEXT_H
+
+
+class Text {
+
+};
+
+
+#endif //PORR_TEXT_H
