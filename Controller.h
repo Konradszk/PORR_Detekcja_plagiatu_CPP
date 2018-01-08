13e8876b3281687c3d64289de3f99f4893956ebc
KarolinaCabaj <karolina.anna.cabaj@gmail.com>
2018-01-08 21:01:28 +0100

    refactor

--- Controller.h
+++ Controller.h
@@ -16,16 +16,16 @@ public:
 
 private:
 public:
-    Controller( Text &baseText,  Text &examinedText,  Text &wordsToCheckText);
+    Controller( Text &base_text,  Text &examined_text,  Text &words_to_check_text);
     void check();
 private:
-    Text baseText, examinedText, wordsToCheckText;
+    Text base_text, examined_text, words_to_check_text;
     std::list<std::string> words;
 
     std::map<std::string, int> minimum(std::map<std::string, int> a, std::map<std::string, int> b);
 
     std::map<std::string, int> repeats(std::list<std::string> text);
-    int sumRepeats(std::map<std::string, int> min_map);
+    int sum_repeats(std::map<std::string, int> min_map);
 };
 
 
--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ int main() {
 
     auto start = std::chrono::system_clock::now();
 
-    Text baseText = Text("C:\\Users\\Lenovo\\CLionProjects\\PORR\\6_image.txt");
-    Text examinedText = Text("C:\\Users\\Lenovo\\CLionProjects\\PORR\\2_image.txt");
-    Text wordsToCheckText = Text("C:\\Users\\Lenovo\\CLionProjects\\PORR\\check_image_recognition.txt");
-    Controller controller =  Controller(baseText, examinedText, wordsToCheckText);
+    Text base_text = Text("C:\\Users\\Lenovo\\CLionProjects\\PORR\\6_image.txt");
+    Text examined_text = Text("C:\\Users\\Lenovo\\CLionProjects\\PORR\\2_image.txt");
+    Text words_to_check_text = Text("C:\\Users\\Lenovo\\CLionProjects\\PORR\\check_image_recognition.txt");
+    Controller controller =  Controller(base_text, examined_text, words_to_check_text);
     controller.check();
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end-start;
